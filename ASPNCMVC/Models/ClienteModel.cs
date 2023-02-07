6bb0b4a882fd83611f31d1cb357a6ee224ccb9a9
Alejandro Bajo <alejandrobajo@proturhotels.com>
2023-02-07 11:39:39 +0100

    rename branch

--- ASPNCMVC/Models/ClienteModel.cs
+++ ASPNCMVC/Models/ClienteModel.cs
@@ -7,7 +7,7 @@
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
         public string NombreFiscal { get; set; } 
-        public string Nif { get; set; }
+        public string Nif { get; set; } 
         public string Mail { get; set; }
         public string Movil { get; set; }
         public string Telefono { get; set; }
